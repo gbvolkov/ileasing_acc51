62e77732eb4e34eebf1bbc0c216a1e357c3318a6
Georgy <gbvolkov@gmail.com>
2023-07-23 20:31:07 +0300

    1

--- const.py
+++ const.py
@@ -1,3 +1,3 @@
 COLUMNS = ["clientID", "clientBIC", "clientBank", "clientAcc", "clientTaxCode", "clientName", "stmtDate", "stmtFrom", "stmtTo", "openBalance", "totalDebet", "totalCredit", "closingBalance",
            "entryDate", "cpBIC", "cpBank", "cpAcc", "cpTaxCode", "cpName", "Debet", "Credit", "Comment",
-           "toIgnore", "filename"]
+           "toIgnore", "filename", 'processdate']
